d52022c549c3350ba2be8f491a99811344874565
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-09 17:35:56 +0100

    completed project sketch

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import Logo from '../assets/investment-calculator-logo.png'
 
 export default function Header() {
     return (
-        <header>
+        <header id='header'>
             <img src={Logo}/>
             <h1>Investment Calculator</h1>
         </header>
