71df062ba55e4a5ef8673c6faedbadffa2a189bc
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-09 17:19:20 +0100

    updated investment header

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import Logo from '../assets/investment-calculator.png'
+
+export default function Header() {
+    return (
+        <header>
+            <img src={Logo}/>
+            <h1>Investment Calculator</h1>
+        </header>
+    )
+}
