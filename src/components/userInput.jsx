ce4b6b18b32bbcae7e43855e3c2cec5c765090d3
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-09 17:52:02 +0100

    inserted the simply logic into the userInput component

--- src/components/userInput.jsx
+++ src/components/userInput.jsx
@@ -5,28 +5,66 @@ export default function UserInput() {
     InitialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
-    duration: 10
+    duration: 10,
   });
+
+  function handleChange(newValue, inputIdentifier) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.InitialInvestment}
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestmentInvestment}
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) =>
+              handleChange("duration", event.target.value)
+            }
+          />
         </p>
       </div>
     </section>
