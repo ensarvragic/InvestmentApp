e88a6aecd112986fd7a5d1cbdde5f7881730e217
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-09 17:55:27 +0100

    changed the values of font-size into css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './components/header'
 import './App.css'
+import UserInput from './components/userInput'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <UserInput/>
     </>
   )
 }
