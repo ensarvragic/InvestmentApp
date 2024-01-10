37fba378f6846813be77c929b1ea131f2e68d3d2
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-10 16:29:15 +0100

    lifting up the state from userInput into app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import Header from './components/header'
+import Header from './components/Header'
 import './App.css'
 import UserInput from './components/userInput'
 
 function App() {
 
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header/>
-      <UserInput/>
+      <UserInput onChangeInput={handleChange}/>
     </>
   )
 }
